148fcfc437ebd63ba53a3ea4587a1e978c7710bd
Yajushi Srivastava <yajushi.sri@gmail.com>
2020-10-25 16:51:54 +0530

    Adds feature for multiple badge generation on a pull request

--- main.js
+++ main.js
@@ -29,8 +29,6 @@ async function main() {
         commit_sha: sha,
     });
     const pr_number = getPRnumber.data[0].number;
-    console.log("RESPONSE getPRnumber ", getPRnumber);
-    console.log("pr_number: ", pr_number);
     // get pull request data 
     const { data: pullRequest } = await octokit.pulls.get({
         owner: repo.split("/")[0],
@@ -38,117 +36,116 @@ async function main() {
         pull_number: pr_number
     });
     const githubUsername = pullRequest.user.login;
-    console.log("RESPONSE pullRequest ", pullRequest);
-    console.log("githubUsername: ", githubUsername);
-    const labelsArray = new Array();  
-    for (i in pullRequest.labels){
+    console.log("GitHub Username: ", githubUsername);
+    const labelsArray = new Array();
+    for (i in pullRequest.labels) {
         labelsArray.push(pullRequest.labels[i].name);
     }
     // get user data 
     const { data: userData } = await octokit.users.getByUsername({
         username: githubUsername,
-      });
+    });
     const fullName = userData.name;
     const userEmail = userData.email;
     const pr_url = pullRequest.html_url;
-    console.log("RESPONSE userData ", userData);
-    console.log("fullName: ", fullName);
-    console.log("userEmail ", userEmail);
-    console.log("pr_url: ", pr_url);
+    console.log("Full Name: ", fullName);
+    console.log("Email ", userEmail);
+    console.log("Evidence URL: ", pr_url);
     var options = {
         method: 'POST',
         uri: 'https://api.eu.badgr.io/o/token',
         form: {
-            username: username, 
+            username: username,
             password: password
         },
         headers: {
-            'Content-Type' : 'application/json'
+            'Content-Type': 'application/json'
         }
     };
-     
+
     accessToken = await rp(options)
-        .then(function (body) {  
+        .then(function (body) {
             var response = JSON.parse(body);
-            accessToken = response.access_token;  
+            accessToken = response.access_token;
             return accessToken;
         })
-        .catch(function (err) { 
+        .catch(function (err) {
             console.log(err)
         });
-
-    console.log("ACCESS TOKEN: ", accessToken);
 
     var options = {
         method: 'GET',
         uri: 'https://api.eu.badgr.io/v2/badgeclasses',
         headers: {
-            'Content-Type' : 'application/json',
-            'Authorization' : 'Bearer ' + accessToken
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
         }
     };
-
-    badgeDetails = await rp(options)
+    const badgeDetailsArray = new Array();
+    await rp(options)
         .then(function (body) {
             var responseAllBadges = JSON.parse(body);
-            for(var i=0; i<labelsArray.length; i++){
-                for(var j=0; j<responseAllBadges.result.length; j++){
+            for (var i = 0; i < labelsArray.length; i++) {
+                for (var j = 0; j < responseAllBadges.result.length; j++) {
                     var badge = responseAllBadges.result[j].name.toLowerCase();
                     var label = labelsArray[i].replace('"', '').replace('"', '').toLowerCase();
-                    if(badge.includes(label)){
+                    if (badge.includes(label)) {
                         console.log(responseAllBadges.result[j]);
-                        return responseAllBadges.result[j];
+                        badgeDetailsArray.push(responseAllBadges.result[j]);
                     }
-                }        
-            }            
+                }
+            }
         });
-    console.log("RESPONSE badgeDetails ", badgeDetails);
-    console.log("RESPONSE badgeDetails typeof ", typeof badgeDetails);
-    console.log("RESPONSE badgeDetails entity id ", badgeDetails.entityId);
+    if (Array.isArray(badgeDetailsArray) && badgeDetailsArray.length > 0) {
+        for (i in badgeDetailsArray) {
+            var options = {
+                method: 'POST',
+                uri: 'https://api.eu.badgr.io/v2/badgeclasses/' + badgeDetailsArray[i].entityId.toString() + '/assertions',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accessToken
+                },
+                json: true,
+                body: {
+                    recipient: {
+                        identity: userEmail,
+                        hashed: true,
+                        type: "email",
+                        plaintextIdentity: fullName
+                    },
+                    issuedOn: new Date().toISOString(),
+                    evidence: [
+                        {
+                            url: pr_url,
+                            narrative: badgeDetailsArray[i].criteriaNarrative
+                        }
+                    ],
+                    notify: true,
+                    extensions: {
+                        "extensions:recipientProfile": {
+                            "@context": "https://openbadgespec.org/extensions/recipientProfile/context.json",
+                            "type": [
+                                "Extension",
+                                "extensions:RecipientProfile"
+                            ],
+                            name: fullName
+                        }
+                    }
+                }
+            };
 
-    var options = {
-        method: 'POST',
-        uri: 'https://api.eu.badgr.io/v2/badgeclasses/' + badgeDetails.entityId.toString() + '/assertions',
-        headers: {
-            'Content-Type' : 'application/json',
-            'Authorization' : 'Bearer ' + accessToken
-        },
-        json: true,
-        body: {
-            recipient: {
-                identity: userEmail,
-                hashed: true,
-                type: "email",
-                plaintextIdentity: fullName
-            },
-            issuedOn: new Date().toISOString(),
-            evidence: [
-                {
-                    url: pr_url,
-                    narrative: badgeDetails.criteriaNarrative
-                }
-            ],
-            notify: true,
-            extensions: {
-                "extensions:recipientProfile": {
-                  "@context": "https://openbadgespec.org/extensions/recipientProfile/context.json",
-                  "type": [
-                    "Extension",
-                    "extensions:RecipientProfile"
-                  ],
-                  name: fullName
-                }
-              }
+            await rp(options)
+                .then(function (body) {
+                    console.log("The following badge was generated for the specified recipient:\n ", body);
+                })
+                .catch(function (err) {
+                    console.log(err)
+                });
         }
-    };
-
-    await rp(options)
-    .then(function (body) {  
-        console.log("ASSERT BADGE: ", body);
-    })
-    .catch(function (err) { 
-        console.log(err)
-    });
+    } else {
+        console.log("The badge(s) generation failed for the specified recipient");
+        break;
+    }
 }
 
 main().catch(error => setFailed(error.message));
